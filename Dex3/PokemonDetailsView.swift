9d43c070af4663e3661a7579599c4b6f29d8961d
someDori <demetrepanjakidze1@gmail.com>
2024-12-08 19:52:07 +0400

    feat: create part of the PokemonDetailsView

--- Dex3/PokemonDetailsView.swift
+++ Dex3/PokemonDetailsView.swift
@@ -10,9 +10,55 @@ import CoreData
 
 struct PokemonDetailsView: View {
     @EnvironmentObject var pokemon: Pokemon
+    @State private var showShiny = false
     
     var body: some View {
-        Text(pokemon.description)
+        ScrollView {
+            ZStack {
+                Image("normalgrasselectricpoisonfairy")
+                    .resizable()
+                    .scaledToFit()
+                    .shadow(color: .black, radius: 6)
+                AsyncImage(url: showShiny ? pokemon.shiny : pokemon.sprite) { image in
+                    image
+                        .resizable()
+                        .scaledToFit()
+                        .padding(.top, 50)
+                        .shadow(color: .black, radius: 6)
+                } placeholder: {
+                    ProgressView()
+                }
+                
+            }
+            HStack {
+                ForEach(pokemon.types!, id: \.self) { type in
+                    Text(type.capitalized)
+                        .font(.title2)
+                        .shadow(color: .white, radius: 1)
+                        .padding(.vertical, 7)
+                        .padding(.horizontal)
+                        .background(Color(type.capitalized))
+                        .cornerRadius(50)
+                }
+                Spacer()
+            }
+            .padding()
+        }
+        .navigationTitle(pokemon.name!.capitalized)
+        .toolbar {
+            ToolbarItem(placement: .topBarTrailing) {
+                Button {
+                    showShiny.toggle()
+                } label: {
+                    if showShiny {
+                        Image(systemName: "wand.and.stars")
+                            .foregroundColor(.yellow)
+                    } else {
+                        Image(systemName: "wand.and.stars.inverse")
+                    }
+                }
+            }
+        }
     }
 }
 
